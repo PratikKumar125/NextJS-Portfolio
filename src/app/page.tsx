fb29c818dfa5437c6733bf1e34472069bd8968ef
PratikKumar125 <prateektiwari378@gmail.com>
2024-11-09 22:19:50 +0530

    ADD:Initital setup

---
+++ src/app/page.tsx
@@ -0,0 +1,332 @@
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import IconCloud from "@/components/ui/icon-cloud";
+import PulsatingButton from "@/components/ui/pulsating-button";
+import { DATA } from "@/data/resume";
+import { Download } from "lucide-react";
+import Link from "next/link";
+import Markdown from "react-markdown";
+
+const BLUR_FADE_DELAY = 0.04;
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "react",
+  "redux",
+  "reactquery",
+  "android",
+  "html5",
+  "css3",
+  "tailwind",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "nestjs",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "docker",
+  "git",
+  "github",
+  "docker",
+  "kubernetes",
+  "queues",
+  "redis",
+  "golang",
+  "python",
+  "serverless",
+  "graphql",
+  "airbyte",
+  "jenkins",
+  "dbt",
+  "mongodb",
+  "bigquery"
+];
+
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm Pratik 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+              <BlurFade delay={BLUR_FADE_DELAY}>
+                <PulsatingButton className="mt-2">
+                  <a className="flex gap-x-1" href="/pratik_resume.pdf" download={"Pratik_CV.pdf"}>
+                    Download Resume <span> <Download className="p-1" /> </span>
+                  </a>
+                </PulsatingButton>
+              </BlurFade>
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <div className="flex gap-x-4 my-2">
+            {
+              DATA.about?.map((about) => {
+                return <p className="flex items-center gap-x-1 text-sm font-bold"><span> {about?.icon} </span> {about?.value}</p>
+              })
+            }
+          </div>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+                isDefaultExpanded={true}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+
+      <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+        <div className="relative flex size-full w-full items-center justify-center overflow-hidden rounded-lg bg-background px-20 pt-8 ">
+          <IconCloud iconSlugs={slugs} />
+        </div>
+      </section>
+
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section id="blogs">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Blogs
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest writtings
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve explored a range of topics, from beginner-friendly guides to deep technical dives. Here are a few of my favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.blogs.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      {/* <section id="hackathons">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 13}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Hackathons
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  I like building things
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  During my time in university, I attended{" "}
+                  {DATA.hackathons.length}+ hackathons. People from around the
+                  country would come together and build incredible things in 2-3
+                  days. It was eye-opening to see the endless possibilities
+                  brought to life by a group of motivated and passionate
+                  individuals.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 14}>
+            <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+              {DATA.hackathons.map((project, id) => (
+                <BlurFade
+                  key={project.title + project.dates}
+                  delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                >
+                  <HackathonCard
+                    title={project.title}
+                    description={project.description}
+                    location={project.location}
+                    dates={project.dates}
+                    image={project.image}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </ul>
+          </BlurFade>
+        </div>
+      </section> */}
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me a mail at{" "}
+                <Link
+                  href={DATA.contact.email}
+                  className="text-blue-500 hover:underline"
+                >
+                  prateektiwari378@gmail.com
+                </Link>{" "}
+                and I&apos;ll respond 😊.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,305 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, Mail, MapPin } from "lucide-react";
+
+export const DATA = {
+  name: "Pratik Kumar",
+  initials: "PK",
+  url: "https://dillion.io",
+  location: "Gurugram, India",
+  locationLink: "",
+  description:
+    "Software Engineer with a passion to build great products and helping people.",
+  summary:
+    // "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "I’m a full-stack developer and DevOps enthusiast with a strong foundation in building high-quality web applications and cloud infrastructure. I graduated in 2024 with a CGPA of 8.3 and bring around 1.5 years of experience working on industry-grade projects. My skill set includes trending frontend frameworks like NextJS and NestJS with AWS, Kubernetes, Docker, CI/CD, and GitOps, along with an eye for scalable, efficient solutions. Whether it’s creating robust ELT pipelines, setting up automated workflows, or developing user-focused interfaces, I’m passionate about delivering impactful, high-performance solutions that enhance user experiences.",
+  avatarUrl: "/me.jpeg",
+  skills: [
+    "React",
+    "React Query",
+    "Next.js",
+    "Javascript",
+    "Typescript",
+    "Tailwind",
+    "Node.js",
+    "Nest.js",
+    "Golang",
+    "Python",
+    "Postgres",
+    "MongoDB",
+    "GraphQL",
+    "Docker",
+    "Kubernetes",
+    "AWS",
+    "Serverless",
+    "Jenkins",
+    "Airbyte",
+    "DBT",
+    "ELT",
+    "Redis",
+    "Queues",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    // { href: "https://medium.com/@prateektiwari378", icon: NotebookIcon, label: "Blog" },
+  ],
+  about: [
+    {
+      icon: <Mail className="p-1" />,
+      value: 'prateektiwari378@gmail.com'
+    },
+    {
+      icon: <MapPin className="p-1" />,
+      value: 'Gurugram'
+    },
+  ],
+  contact: {
+    email: "prateektiwari378@gmail.com",
+    tel: "+918978936442",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/PratikKumar125",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://linkedin.com/in/pratikkumar22",
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      Medium: {
+        name: "LinkedIn",
+        url: "https://medium.com/@prateektiwari378",
+        icon: Icons.medium,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Squareboat Solutions",
+      href: "https://squareboat.com/",
+      badges: [],
+      location: "Gurugram",
+      title: "Software Engineer L1",
+      logoUrl: "/sqb.png",
+      start: "July 2024",
+      end: "Present",
+      description:
+        "Developed scalable data pipelines and integrations for platforms like Google Ads, Shopify, and TikTok, optimizing ETL workflows with tools like Airbyte, DBT, and AWS ECS. Strengthened application security with AWS Secrets Manager and Serverless integrations. Additionally, contributed to mobile and web development as a frontend and React Native developer, creating user-friendly interfaces across ReactJS, Next.js, and Android Studio, enhancing user engagement and experience across multiple platforms.",
+    },
+    {
+      company: "Squareboat Solutions",
+      badges: [],
+      href: "https://squareboat.com/",
+      location: "Gurugram",
+      title: "Software Engineer L0",
+      logoUrl: "/sqb.png",
+      start: "July 2023",
+      end: "July 2024",
+      description:
+        "Built full-stack solutions with NestJS, ReactJS. Integrated notifications system with support for FCM, Slack and Email from scratch using AWS-SQS. Added RBAC & ABAC at the full-stack level for enhanced authorization. Led the frontend revamp leading to user engagement up by 700%. Built a CSV file export and upload feature, utilizing AWS S3 for efficient uploads, scalability, and seamless integrations.",
+    },
+  ],
+  education: [
+    {
+      school: "GLA University",
+      href: "https://www.gla.ac.in/",
+      degree: "B-Tech",
+      logoUrl: "/gla.png",
+      start: "2020",
+      end: "2024",
+    },
+  ],
+  projects: [
+    {
+      title: "The Storyteller",
+      href: "https://thestoryteller.tech/",
+      dates: "Oct 2023 - Aug 2024",
+      active: true,
+      description:
+        "A powerful AI-driven platform transforming ad campaigns with audience discovery, creative fatigue prediction, and competitor insights. Led full-stack development, including a complete frontend overhaul that boosted user engagement by over 700%, implemented FCM, Slack, and email notifications, and enforced RBAC and ABAC controls.",
+      technologies: [
+        "React.js",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Typescript",
+        "NestJS",
+        "MongoDB",
+        "Bigquery",
+        "Airbyte",
+        "Queues",
+        "Redis"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://thestoryteller.tech/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/storyteller.png",
+      video:
+        "",
+    },
+    {
+      title: "Regisedge",
+      href: "https://demoapp.regisedge.com/",
+      dates: "Sept 2024 - Present",
+      active: true,
+      description:
+        "Developed a robust ELT pipeline handling data from platforms like Google Ads, Shopify, Facebook, and TikTok using Airbyte. Automated dbt transformations with AWS ECS, integrated Secrets Manager with Lambda, and streamlined operations with the Serverless Framework.",
+      technologies: [
+        "NestJS",
+        "Typescript",
+        "MongoDB",
+        "Airbyte",
+        "DBT",
+        "Bigquery",
+        "Serverless",
+        "AWS-ECS",
+        "AWS",
+        "Redis",
+        "Docker",
+        "CI-CD",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://demoapp.regisedge.com/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/regisedge.png",
+      video: "",
+    },
+    {
+      title: "GO-Gopher",
+      href: "https://github.com/PratikKumar125/go-gopher",
+      dates: "August 2024 - Present",
+      active: true,
+      description:
+        "Open-source production-ready Go boilerplate featuring CRON jobs, Redis integration, MongoDB, pagination, and modular architecture with dependency injection using Uber's Dig.",
+      technologies: [
+        "Golang",
+        "Redis",
+        "MongoDB",
+        "AWS-SQS",
+        "AWS-S3",
+        "CRON",
+        "Dependency Injection (Dig)",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/PratikKumar125/go-gopher",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/golang.gif",
+      video: "",
+    },
+    {
+      title: "Via-Fans",
+      href: "",
+      dates: "April 2024 - present",
+      active: true,
+      description:
+        "A platform for creators to connect with their audience through exclusive content. Designed and implemented secure media storage, real-time data handling, efficient caching, and seamless payment processing. Built to scale with a user-friendly interface that supports high-performance and reliability.",
+      technologies: [
+        "Next.js",
+        "TailwindCSS",
+        "Typescript",
+        "React Query",
+        "PostgreSQL",
+        "AWS-S3",
+        "AWS-SQS",
+        "Redis",
+        "Lemon Squeezy",
+        "Serverless",
+        "Kubernetes",
+        "Docker",
+      ],
+      links: [
+        // {
+        //   type: "Website",
+        //   href: "https://automatic.chat",
+        //   icon: <Icons.globe className="size-3" />,
+        // },
+      ],
+      image: "/fans.png",
+      video:
+        "",
+    },
+  ],
+  blogs: [
+    {
+      title: "NestJS Monorepo to Kubernetes with MongoDB Statefulset.",
+      href: "/",
+      dates: "Aug 2024",
+      active: true,
+      description:
+        "In this blog post, I’ll walk you through the process of migrating a NestJS monorepo application to Kubernetes, using Minikube for local development. The project consists of two monorepo :- control-panel and auth, both of which rely on MongoDB for data storage and Redis for caching. The MongoDB instance is set up with a master-slave architecture using a StatefulSet, while Redis is deployed as a single pod.",
+      technologies: [
+        "NestJS",
+        "MongoDB",
+        "Docker",
+        "Kubernetes"
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://medium.com/@prateektiwari378/nestjs-monorepo-to-kubernetes-with-mongodb-statefulset-a5dbaeb1ecd2",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "",
+    },
+    {
+      title: "NestJS + GraphQL: A Comprehensive Guide to Code-First API Design",
+      href: "/",
+      dates: "Sept 2024",
+      active: true,
+      description:
+        "GraphQL is a query language for APIs that allows clients to request exactly the data they need, making it highly efficient and flexible. Unlike REST, which requires multiple endpoints for different resources, GraphQL consolidates everything into a single endpoint where clients can specify the structure of the response.",
+      technologies: [
+        "NestJS",
+        "GraphQL",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://medium.com/@prateektiwari378/nestjs-graphql-a-comprehensive-guide-to-code-first-api-design-9064a581fc10",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "",
+    },
+  ],
+  hackathons: [
+    // {
+    //   title: "Hack Western 5",
+    //   dates: "November 23rd - 25th, 2018",
+    //   location: "London, Ontario",
+    //   description:
+    //     "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+    //   image:
+    //     "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+    //   mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+    //   links: [],
+    // },
+  ],
+} as const;
